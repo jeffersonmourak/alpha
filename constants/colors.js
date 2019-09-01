659ea9a5a2a8b88a01a47cd0dc673a1d512f2cc4
Jefferson Moura <jefinho.moura14@gmail.com>
2019-09-01 14:58:12 -0400

    page 1.0

--- constants/colors.js
+++
@@ -1,8 +0,0 @@
-const COLORS = {
-    blue: '#0d447b',
-    white: '#ffffff',
-    black: '#000000',
-    grey: '#808080',
-};
-
-export default COLORS;
--- constants/key.js
+++
@@ -1,6 +0,0 @@
-/**
- * Key generator.
- */
-const key = () => Math.random().toString(36).substr(3);
-
-export default key;
